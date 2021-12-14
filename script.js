e05b9079443114c26d247115f38f58c615ae268a
Dave Donatuccio <ddonatuccio@gmail.com>
2021-12-13 19:53:33 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function scrollHandler() {
+    var element = document.getElementById('top');
+
+    var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+    var elementHeight = element.offsetHeight;
+    var scrollTop = document.documentElement.scrollTop;
+
+    var opacity = 1;
+
+    if (scrollTop > distanceToTop) {
+        opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+    }
+
+    if (opacity >= 0) {
+        element.style.opacity = opacity;
+    }
+}
+
+window.addEventListener('scroll', scrollHandler);
+
