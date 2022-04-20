06d4e21fb5a942b6f12e1356ffb1d1c00d8c08dc
Dave Donatuccio <ddonatuccio@gmail.com>
2022-04-20 10:08:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function scrollHandler() {
-    var element = document.getElementById('top');
+// function scrollHandler() {
+//     var element = document.getElementById('top');
 
-    var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-    var elementHeight = element.offsetHeight;
-    var scrollTop = document.documentElement.scrollTop;
+//     var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+//     var elementHeight = element.offsetHeight;
+//     var scrollTop = document.documentElement.scrollTop;
 
-    var opacity = 1;
+//     var opacity = 1;
 
-    if (scrollTop > distanceToTop) {
-        opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-    }
+//     if (scrollTop > distanceToTop) {
+//         opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+//     }
 
-    if (opacity >= 0) {
-        element.style.opacity = opacity;
-    }
-}
+//     if (opacity >= 0) {
+//         element.style.opacity = opacity;
+//     }
+// }
 
-window.addEventListener('scroll', scrollHandler);
+// window.addEventListener('scroll', scrollHandler);
 
